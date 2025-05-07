65f7065dec9fb49973d64427659d258cb5b47fc8
PyGitPyHub <85502183+PyGitPyHub@users.noreply.github.com>
2025-05-07 14:32:47 +0200

    Update image_cropper.dart

--- lib/image_cropper.dart
+++ lib/image_cropper.dart
@@ -1,4 +1,4 @@
-///
+/// 
 /// * author: Hung Duy Ha (hunghd)
 /// * email: hunghd.yb@gmail.com
 ///
@@ -10,7 +10,7 @@
 /// Note that: this plugin is based on different native libraries depending on
 /// Android or iOS platform, so it shows different UI look and feel between
 /// those platforms.
-///
+/// Py Version: 2.2
 
 export 'src/cropper.dart';
 export 'src/options.dart';
